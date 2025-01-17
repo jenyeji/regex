ab93801c1a59432ee22c8ea3f46d03c3a9a68d9b
Jennifer Suratna <jenyeji@gmail.com>
2025-01-16 20:02:28 -0800

    fix color

--- src/RegexInput.jsx
+++ src/RegexInput.jsx
@@ -11,7 +11,7 @@ function RegexInput({ pattern, flags, error, onRegexUpdate, onFlagUpdate }) {
           name="regex-pattern"
           value={pattern}
           aria-label="Regex pattern"
-          style={{ color: error ? 'red' : 'fieldtext' }}
+          style={{ color: error ? 'red' : 'white' }}
           onChange={(e) => {
             onRegexUpdate(e.target.value);
           }}
