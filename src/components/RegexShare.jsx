368300cad4b260c17b731cc4a269f6961128e517
Jennifer Suratna <jenyeji@gmail.com>
2025-01-17 18:37:03 -0800

    error handling for share url

--- src/components/RegexShare.jsx
+++ src/components/RegexShare.jsx
@@ -5,6 +5,7 @@ function RegexShare({ shareUrl, pattern, flags, testString, origin, onShare }) {
     <div className="share">
       <button
         aria-label="Share this page"
+        className="share-button"
         onClick={() => {
           const data = {
             pattern,
@@ -14,6 +15,12 @@ function RegexShare({ shareUrl, pattern, flags, testString, origin, onShare }) {
           const compressedData = encodeURIComponent(
             LZString.compressToBase64(JSON.stringify(data))
           );
+          if (compressedData.length >= 2048) {
+            alert(
+              'Your input is too long. Please reduce the length of your inputs (regex and/or test string) to share.'
+            );
+            return;
+          }
           onShare(compressedData);
         }}
       >
@@ -21,7 +28,7 @@ function RegexShare({ shareUrl, pattern, flags, testString, origin, onShare }) {
       </button>
       {shareUrl && (
         <div className="share-url">
-          Shared URL:{' '}
+          This URL captures and preserves the current state of the Regex app:{' '}
           <a href={`${origin}?json=${shareUrl}`}>
             {`${origin}?json=${shareUrl}`}
           </a>
